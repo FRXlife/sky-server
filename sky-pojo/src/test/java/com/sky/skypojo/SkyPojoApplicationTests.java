db62e0cdbcb6ae6a73d4b406c78d92355c9c2841
frx <frx@openfunds.ai>
2024-04-26 11:42:45 +0800

    项目初始化

---
+++ sky-pojo/src/test/java/com/sky/skypojo/SkyPojoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.skypojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyPojoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
